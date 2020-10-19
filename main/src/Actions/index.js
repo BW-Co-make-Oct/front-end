2111f22c4700728a3e7a1df84c0fcf896545bcf2
Reuben Palumbo <reubensandwichthe1st@gmail.com>
2020-10-19 10:56:43 -0800

    Added actions,reduces

---
+++ main/src/Actions/index.js
@@ -0,0 +1,7 @@
+import { axiosWithAuth } from "../utils/axoisWithAuth";
+
+export const GET_DATA = "GET_DATA";
+export const DELETE_POST = "DELETE_POST";
+export const ADD_POST = "ADD_POST";
+export const EDIT_POST = "EDIT_POST";
+export const SET_VOTES = "SET_VOTES";
